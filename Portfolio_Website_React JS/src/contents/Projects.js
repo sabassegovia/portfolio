0e0328aa162e2504d4b674f3a5e2c570402fede1
Sabas Segovia <sasegovia@ucdavis.edu>
2022-06-16 11:24:10 -0700

    feat: deploy with scriptly as a test

--- Portfolio_Website_React JS/src/contents/Projects.js
+++ Portfolio_Website_React JS/src/contents/Projects.js
@@ -6,6 +6,8 @@ class Projects extends Component {
         return (
             <div className="condiv">
                 Page in progress...Check back soon...
+                <br></br>
+                <h2> <a className="linkStyle" href="https://script-ly.vercel.app/" target="_blank" rel="noopener noreferrer">Script.ly</a></h2>
             <Social />
             </div>
             )
