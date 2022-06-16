1e7b3a3560a0fd1b1af5764b0f6d0c35f10dcf6f
Sabas Segovia <sasegovia@ucdavis.edu>
2022-06-16 02:29:25 -0700

    Updates

---
+++ precache-manifest.ac5e6a2c87c1c8bae17dde69c7ac6fac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40851e63abdb2aafd38df87f4ace2855",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "744c0e5773a87bd69b00",
+    "url": "/portfolio/static/css/main.51b11c40.chunk.css"
+  },
+  {
+    "revision": "060aab83dcae9be7115b",
+    "url": "/portfolio/static/js/2.2e45242d.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/portfolio/static/js/2.2e45242d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "744c0e5773a87bd69b00",
+    "url": "/portfolio/static/js/main.2be3acdd.chunk.js"
+  },
+  {
+    "revision": "72153f1981be4ee14a4e",
+    "url": "/portfolio/static/js/runtime-main.55975c6a.js"
+  },
+  {
+    "revision": "7c616561354c1eab98fef2c914459bc0",
+    "url": "/portfolio/static/media/tempBG.7c616561.png"
+  }
+]);
