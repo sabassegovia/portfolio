c5d4f20a73990b7d9f55ca74cfdd624323799414
Sabas Segovia <sasegovia@ucdavis.edu>
2022-06-16 15:43:18 -0700

    feat: tyding up projects.js

--- Portfolio_Website_React JS/src/contents/Projects.js
+++ Portfolio_Website_React JS/src/contents/Projects.js
@@ -5,6 +5,7 @@ class Projects extends Component {
     render() {
         return (
             <div className="condiv">
+                 <h1 className="subtopic">My Projects</h1>
                 Page in progress...Check back soon...
                 <br></br>
                 <h2> <a className="linkStyle" href="https://script-ly.vercel.app/" target="_blank" rel="noopener noreferrer">Script.ly</a></h2>
