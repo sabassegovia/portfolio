15961309548c54aef8b9204df3449bc4f2723831
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-06 23:54:31 -0700

    feat: completed cards for projects

--- Portfolio_Website_React_JS/src/contents/Projects.js
+++ Portfolio_Website_React_JS/src/contents/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Social from '../components/Social';
+import data from '../data/project-data.js';
+import ProjectCard from '../components/ProjectCard.js';
 
 class Projects extends Component {
     render() {
@@ -7,42 +9,10 @@ class Projects extends Component {
             <div className="condiv">
                 <h1 className="subtopic">My Projects</h1>
                 <br></br>
-                <h2>
-                    <a className="linkStyle" href="http://getscriptly.com/" target="_blank" rel="noopener noreferrer">Script.ly</a>
-                    </h2>
-                <h2>
-                    <a className="linkStyle" href="https://sabassegovia.github.io/bartendersfriend" target="_blank" rel="noopener noreferrer">Bartenders Friend</a>
-                    </h2>
-                <h2>
-                    <a className="linkStyle" href="https://nextjs-practice-deploy-git-main-sabassegovia.vercel.app/" target="_blank" rel="noopener noreferrer">Notes.App</a>
-                </h2>
-                <h2>
-                    <a className="linkStyle" href="https://sabassegovia.github.io/sprite_animations/" target="_blank" rel="noopener noreferrer">Vanilla Javascript Sprite Animations</a>
-                </h2>
-                <h2>
-                    <a className="linkStyle" href="https://sabassegovia.github.io/managing-state/" target="_blank" rel="noopener noreferrer">Responsive Sprite Animations
-                    </a>
-                </h2>
-                <h2>
-                    <a className="linkStyle" href="https://sabassegovia.github.io/enemies/" target="_blank" rel="noopener noreferrer">Enemy Sprite Animations CSS
-                    </a>
-                </h2>
-                <h2>
-                    <a className="linkStyle" href="https://sabassegovia.github.io/moving-background/" target="_blank" rel="noopener noreferrer">CSS Paralax Background
-                    </a>
-                </h2>
-                <h2>
-                    <a className="linkStyle" href="https://sabassegovia.github.io/bat-shooter/" target="_blank" rel="noopener noreferrer">Bat Shooter &#40;Desktop Only&#41;
-                    </a>
-                </h2>
-                <h2>
-                    <a className="linkStyle" href="https://sabassegovia.github.io/jump-jump-scroller/" target="_blank" rel="noopener noreferrer">Jump Jump: A 2-D Sprite Scroller w/Mobile Support
-                    </a>
-                </h2>
-
+                {data.map(project => {
+                   return <ProjectCard projectUrl={project.projectUrl} projectName={project.projectName} />
+                })};
             <Social />
-
-
             </div>
             )
         }
